bc35aba36d412bb26393a0c25605931381984f9e
yang.dongping <18267094443@163.com>
2022-03-25 17:30:19 +0800

    修改

--- 0.1/src/index.js
+++ 0.1/src/index.js
@@ -1,13 +1,18 @@
-// import $ from 'jquery'
+import $ from 'jquery'
 
-// $('#title').click(() => {
-//   $('body').css('backgroundColor', 'deeppink')
-// })
+$('#title').click(() => {
+  $('body').css('backgroundColor', 'deeppink')
+})
 
 import './index.less'
+
+import img from './assets/Snipaste_2022-03-25_00-07-05.png'
 
 function add(x, y) {
   return x + y
 }
+
+const iconDom = document.querySelector('.icon3')
+iconDom.src = img
 
 console.log(add(1, 2))
